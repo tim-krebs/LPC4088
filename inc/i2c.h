91eadd8775835b76d8df97aea066fa846c553015
Tim Krebs <48254824+tim5672s@users.noreply.github.com>
2020-04-16 08:53:59 +0200

    Update i2c.h

--- inc/i2c.h
+++ inc/i2c.h
@@ -2,7 +2,7 @@
  * Project     : LPC4088FET208
  * Author      :
  * Version     :
- * Copyright   : $(ISAT)
+ * Copyright   : 
  * Description : I2C definition
  **********************************************************************************************************/
 
@@ -20,14 +20,14 @@ public:
 	******************************************************************************/
 	inline void Init(void){
 
-		LPC_SC->PCONP |= (1 << 19);								// Power & Clock: enabled
+		LPC_SC->PCONP |= (1 << 19);							// Power & Clock: enabled
 		LPC_IOCON->P0_0 |= (0x03 << 0);							//I2C_SDA
 		LPC_IOCON->P0_1 |= (0x03 << 0);							//I2C_SCL
 
 		LPC_I2C1->CONCLR = (0x01 << 2) 							// Assert acknowledge clear bit
-						 | (0x01 << 3) 							// i2c clear bit
-						 | (0x01 << 5) 							// Start flag clear bit
-						 | (0x01 << 6);							// i2c interface disable bit
+						 | (0x01 << 3) 					// i2c clear bit
+						 | (0x01 << 5) 					// Start flag clear bit
+						 | (0x01 << 6);					// i2c interface disable bit
 		LPC_I2C1->SCLL   = 0;
 		LPC_I2C1->SCLH   = 5;
 		LPC_I2C1->CONSET = (0x01 << 6); 						// i2c enable
@@ -42,9 +42,9 @@ public:
 
 	    LPC_I2C1->CONCLR = (0x01 << 3);							// i2c interrpt clear bit
 	    LPC_I2C1->CONSET = (0x01 << 5);							// start flag clear bit
-	    while(((LPC_I2C1->CONSET) & (0x01 << 3)) == 0);			// wait till flag cleared
+	    while(((LPC_I2C1->CONSET) & (0x01 << 3)) == 0);					// wait till flag cleared
 	    LPC_I2C1->CONCLR = (0x01 << 5);							// clear start flag bit
-	    return LPC_I2C1->STAT;									// return status
+	    return LPC_I2C1->STAT;								// return status
 	}
 
 	/******************************************************************************
@@ -54,10 +54,10 @@ public:
 	******************************************************************************/
 	inline void Stop(void){
 
-	    LPC_I2C1->CONCLR = (0x01 << 3); 						// clear interrupt flag
+	    LPC_I2C1->CONCLR = (0x01 << 3); 							// clear interrupt flag
 	    LPC_I2C1->CONSET = (0x01 << 4);							// set stop flag
 
-	    while(((LPC_I2C1->CONSET) & (1 << 4)) == 0);			// wait till stop flag cleared
+	    while(((LPC_I2C1->CONSET) & (1 << 4)) == 0);					// wait till stop flag cleared
 	}
 
 	/******************************************************************************
@@ -67,11 +67,11 @@ public:
 	******************************************************************************/
 	inline int TransmitByte(unsigned char data){
 
-		LPC_I2C1->DAT = data;									// holds transmit data
-	    LPC_I2C1->CONCLR = (0x01 << 3); 						// clear interrupt flag
-	    while(((LPC_I2C1->CONSET) & (0x01 << 3)) == 0);			// wait till interrupt flag cleared
+		LPC_I2C1->DAT = data;								// holds transmit data
+	    LPC_I2C1->CONCLR = (0x01 << 3); 							// clear interrupt flag
+	    while(((LPC_I2C1->CONSET) & (0x01 << 3)) == 0);					// wait till interrupt flag cleared
 
-	    return LPC_I2C1->STAT;									//return status
+	    return LPC_I2C1->STAT;								//return status
 	}
 
 	/******************************************************************************
@@ -83,15 +83,15 @@ public:
 	inline int ReceiveByte(unsigned char *ptr, int temp){
 
 	    if(temp){
-	    	LPC_I2C1->CONSET = (0x01 << 2);						// assert acknowledge flag
+	    	LPC_I2C1->CONSET = (0x01 << 2);							// assert acknowledge flag
 	    }
 	    else{
-	    	LPC_I2C1->CONCLR = (0x01 << 2);						// assert acknowledge that bit cleared
+	    	LPC_I2C1->CONCLR = (0x01 << 2);							// assert acknowledge that bit cleared
 	    }
 		LPC_I2C1->CONCLR = (0x01 << 3); 						// clear interrupt flag
-	    while(((LPC_I2C1->CONSET) & (0x01 << 3)) == 0);			// wait till interrupt flag cleared
-	    *ptr = LPC_I2C1->DAT;									// holds receive data
-	    return LPC_I2C1->STAT;									//return status
+	    while(((LPC_I2C1->CONSET) & (0x01 << 3)) == 0);					// wait till interrupt flag cleared
+	    *ptr = LPC_I2C1->DAT;								// holds receive data
+	    return LPC_I2C1->STAT;								//return status
 	}
 
 };
